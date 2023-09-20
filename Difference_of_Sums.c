faf074133beea1668e6313f48a88937350bd97bf
harikakatta08 <143778983+harikakatta08@users.noreply.github.com>
2023-09-20 18:36:01 +0530

    #include<stdio.h> int main() {     int i,n,sum=0,s=0;     scanf("%d",&n);     for(i=1;i<=n;i++){         sum=sum+i;         s=s+(i*i);}         int d=sum*sum;         int c=d-s;             printf("%d ",c);         }

---
+++ Difference_of_Sums.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+    int i,n,sum=0,s=0;
+    scanf("%d",&n);
+    for(i=1;i<=n;i++){
+        sum=sum+i;
+        s=s+(i*i);}
+        int d=sum*sum;
+        int c=d-s;
+            printf("%d ",c);
+        }
+    
